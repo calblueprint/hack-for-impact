43d4e85bf67826db793671ffed51d71fc6ecf5fb
Noah Hernandez <63211322+oahnh@users.noreply.github.com>
2024-01-11 00:18:40 -0800

    feat(faq): adds faq section

---
+++ src/components/FAQDropdown/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import styles from './style.module.scss';
+import { type Question } from '../FAQ.astro';
+
+interface FAQDropdownProps {
+  question: Question;
+  defaultOpen?: boolean;
+}
+
+const FAQDropdown = ({ question, defaultOpen }: FAQDropdownProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(defaultOpen ? true : false);
+  const { label, content, hyperlink } = question;
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const renderContent = () => {
+    const containsLink = ['Is there an event schedule?'].includes(label);
+
+    if (containsLink && hyperlink) {
+      const { target, link } = hyperlink;
+
+      const parts = content.split(target);
+
+      return (
+        <div className={styles.content}>
+          <p>
+            {parts[0]}
+            <a href={link} target="_blank">
+              {target}
+            </a>
+            {parts[1]}
+          </p>
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.content}>
+        <p>{content}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.labelContainer}
+        onClick={handleToggle}
+        tabIndex={0}
+      >
+        <div className={styles.statusIcon}>{isOpen ? '-' : '+'}</div>
+        <b>{label}</b>
+      </div>
+      {isOpen && renderContent()}
+    </div>
+  );
+};
+
+export default FAQDropdown;
