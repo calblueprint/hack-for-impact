a34b07117903299e37e5ff2d0bc5938a199ffc8f
Noah Hernandez <63211322+oahnh@users.noreply.github.com>
2024-01-11 18:14:24 -0800

    [feat] add faq section (#16)
    * feat(faq): adds faq section
    
    * [feat] add button component (#8)
    
    * add graphics, button
    
    * update global button style
    
    * update button styles
    
    * [feat] add nav bar (#9)
    
    * update navbar, h5 global style
    
    * update navbar height
    
    * [feat] add footer (#12)
    
    * add footer
    
    * remove edit to env.d.ts
    
    * use astro image for optimization
    
    * remove edit to env.d.ts
    
    * [feat] add hero section (#11)
    
    * update hero
    
    * update text styles, minor adjustment to hero layout
    
    * remove changes to env.d.ts
    
    * tell prettier to ignore env.d.ts
    
    * tell lint to ignore env.d.ts
    
    * update scroll behavior and padding
    
    * microscopic edits
    
    * add bouncing arrow
    
    * add padding-top to Contact section (#14)
    
    * fix(types): moves question type definition to FAQDropdown component
    
    * [feat] create about section (Our Vision, The Mission) (#13)
    
    * create skeleton
    
    * change div to section
    
    * fill out Our Mission + Vision
    
    * add and use b
    
    * clean up / remove comments
    
    * check style (prettier)
    
    * remove b from global styles
    
    * create div for mission and vision
    
    * feat(faq): adds faq section
    
    * fix(types): moves question type definition to FAQDropdown component
    
    * refactor: addresses review comments
    
    * refactor(faq): adjusts container width, updates keydown functionality
    
    ---------
    
    Co-authored-by: Jinkang Fang <67077248+jinkang-0@users.noreply.github.com>
    Co-authored-by: ccatherinetan <64336644+ccatherinetan@users.noreply.github.com>

---
+++ src/components/FAQDropdown/index.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import styles from './style.module.scss';
+
+export interface Question {
+  label: string;
+  content: string;
+  hyperlink?: { target: string; link: string };
+}
+
+interface FAQDropdownProps {
+  question: Question;
+  defaultOpen?: boolean;
+}
+
+const FAQDropdown = ({ question, defaultOpen }: FAQDropdownProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(defaultOpen ? true : false);
+  const { label, content, hyperlink } = question;
+
+  const handleToggle = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  const renderContent = () => {
+    if (hyperlink) {
+      const { target, link } = hyperlink;
+
+      const parts = content.split(target);
+
+      return (
+        <div className={styles.content}>
+          <p>
+            {parts[0]}
+            <a href={link} target="_blank">
+              {target}
+            </a>
+            {parts[1]}
+          </p>
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.content}>
+        <p>{content}</p>
+      </div>
+    );
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleToggle();
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.labelContainer}
+        onClick={handleToggle}
+        tabIndex={0}
+        onKeyDown={e => handleKeyDown(e)}
+      >
+        <div className={styles.statusIcon}>{isOpen ? '-' : '+'}</div>
+        <b>{label}</b>
+      </div>
+      {isOpen && renderContent()}
+    </div>
+  );
+};
+
+export default FAQDropdown;
