4deb272daebedadbddc2cdd02e6ae62f71b0f035
jinkang-0 <jinkang.fang@gmail.com>
2024-01-11 19:22:44 -0800

    update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://calblueprint.github.io'
+  site: 'https://hackforimpact.calblueprint.org'
 });
